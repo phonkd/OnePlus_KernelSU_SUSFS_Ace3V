be2a64334611a92699bd33f07559c7ad6ad4ff33
Elis <elis.steiner@bedag.ch>
2025-10-24 01:26:52 +0200

    Fix build directory structure and simplify build process

--- build-kernel.sh
+++ build-kernel.sh
@@ -43,7 +43,6 @@ fi
 CONFIG="nord4"
 ANYKERNEL_BRANCH="gki-2.0"
 WORKING_DIR="$(pwd)/$CONFIG"
-KERNELSU_VERSION="main"
 
 # Check for required tools
 REQUIRED_TOOLS="git curl make gcc bc bison flex perl zip python3"
@@ -103,16 +102,14 @@ $REPO sync -c -j$(nproc --all) --no-tags --fail-fast
 # Add KernelSU
 log "Adding KernelSU..."
 cd kernel_platform
-git clone https://github.com/tiann/KernelSU -b main KernelSU
-cd KernelSU
-rm -f kernel/kernel
+git clone https://github.com/tiann/KernelSU -b main
+rm -f KernelSU/kernel/kernel
 
 # Set KernelSU version and configuration
 log "Configuring KernelSU..."
-sed -i 's/ccflags-y += -DKSU_VERSION=16/ccflags-y += -DKSU_VERSION=12321/' kernel/Makefile
+sed -i 's/ccflags-y += -DKSU_VERSION=16/ccflags-y += -DKSU_VERSION=12321/' KernelSU/kernel/Makefile
 
 # Create symlink in kernel tree
-cd ..
 ln -sf $(pwd)/KernelSU/kernel $(pwd)/common/drivers/kernelsu
 echo "obj-y += kernelsu/" >> ./common/drivers/Makefile
 
@@ -131,27 +128,17 @@ sed -i "/stable_scmversion_cmd/s/-maybe-dirty//g" ./build/kernel/kleaf/impl/stam
 sed -i 's/-dirty//' ./common/scripts/setlocalversion
 perl -pi -e 's{UTS_VERSION="\$\(echo \$UTS_VERSION \$CONFIG_FLAGS \$TIMESTAMP \| cut -b -\$UTS_LEN\)"}{UTS_VERSION="#1 SMP PREEMPT Sat Apr 20 04:20:00 UTC 2024"}' ./common/scripts/mkcompile_h
 
-# Prepare build environment
-log "Setting up build environment..."
-cd ..
-rm -rf ./kernel_platform/common/android/abi_gki_protected_exports_*
-git config --global user.email "local-build@localhost"
-git config --global user.name "Local Build"
-
 # Build the kernel
 log "Building the kernel..."
-cd kernel_platform/oplus/build
-if [ $INTERACTIVE -eq 1 ]; then
-    ./oplus_build_kernel.sh pineapple gki
-else
-    echo -e "pineapple\ngki" | ./oplus_build_kernel.sh
-fi
+cd common
+make O=out ARCH=arm64 gki_defconfig
+make O=out ARCH=arm64 -j$(nproc --all)
 
 # Create final ZIP
 log "Creating final ZIP..."
-cd ../../../
-cp ./out/dist/Image ../../AnyKernel3/Image
-cd ../../AnyKernel3
+cd ../../..
+cp $WORKING_DIR/kernel_platform/common/out/arch/arm64/boot/Image AnyKernel3/
+cd AnyKernel3
 zip -r9 "../Anykernel3-OPNord4-A15-6.1-KernelSU.zip" ./*
 
 cd ..
